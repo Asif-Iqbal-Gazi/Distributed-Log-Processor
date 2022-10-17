90c9322dcba8b455781c7bfb2b282dd092bde13a
Asif-Iqbal-Gazi <asif.iqbal.gazi@gmail.com>
2022-10-16 21:36:57 -0500

    Slight Logic Improvement for Better Output Format

--- src/main/scala/com/asif/HelperUtils/ComputeIntervals.scala
+++ src/main/scala/com/asif/HelperUtils/ComputeIntervals.scala
@@ -34,10 +34,11 @@ object ComputeIntervals {
     // Computing the startEpochTime
     val startEpochTime: Long = epochTime - (epochTime % (interval * 1000))
     // Computing the endEpochTime
-    val endEpochTime: Long = startEpochTime + interval * 1000
+    val endEpochTime: Long = (startEpochTime + interval * 1000) - 1
     // convert both the start & end Time to date, so that we can format them
     val startTime: Date = new Date(startEpochTime)
     val endTime: Date = new Date(endEpochTime)
+    //println(outputDateFormat.format(endTime))
     outputDateFormat.format(startTime) + " - " + outputDateFormat.format(endTime)
   }
 }
